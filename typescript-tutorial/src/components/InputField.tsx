a5cf9d667fdb1f1fa0417b06c96fea6fbb2b0415
GrrantSmith121 <70243470+GrrantSmith121@users.noreply.github.com>
2022-06-23 18:05:15 -0400

    Created task text bar

--- typescript-tutorial/src/components/InputField.tsx
+++ typescript-tutorial/src/components/InputField.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import "./styles.css";
 
-const InputField = () => {
-  return <form className = 'input'>
-    <input type = 'input' placeholder = "Enter a task" className = "input_box" />
+interface Props{
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField = ({todo, setTodo, handleAdd}: Props) => {
+  
+  
+  return <form className = 'input' onSubmit={(e) => handleAdd(e)}>
+    <input 
+      type = 'input'
+      value = {todo}
+      onChange = {(e) => setTodo(e.target.value)}
+      placeholder = "Enter a task" className = "input_box" />
     <button className = 'input_submit' type = "submit">
         Go
     </button>
