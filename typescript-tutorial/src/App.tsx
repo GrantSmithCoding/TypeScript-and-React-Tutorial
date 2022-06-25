ff1365966e8eec5b89a8d2125af13af55d8d578d
GrrantSmith121 <70243470+GrrantSmith121@users.noreply.github.com>
2022-06-25 19:04:56 -0400

    Icons not working

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -15,7 +15,7 @@ const App: React.FC = () => {
     e.preventDefault();
 
     if(todo) {
-      setTodos([...todos,{id: Date.now(), todo: todo, isDone: false}]);
+      setTodos([...todos,{id: Date.now(), todo, isDone: false}]);
       setTodo("");
     }
     
